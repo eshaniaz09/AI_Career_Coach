22f4516e817bde2941950f4ebee16d8e9a7149dc
eshaniaz9 <esha_niaz_intern@netixsol.com>
2025-11-25 18:55:26 +0500

    landing page and backend setup

---
+++ app/layout.js
@@ -0,0 +1,45 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "AI Career Coach",
+  description: "AI Platform to help you build your career",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider appearance={{
+      baseTheme:dark,
+    }}>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className}`}
+      >
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">{children} </main>
+            {/* footer */}
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>© 2025 Career AI. Made with 💕 by Eesha.</p>
+              </div>
+            </footer>
+      </ThemeProvider>
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
